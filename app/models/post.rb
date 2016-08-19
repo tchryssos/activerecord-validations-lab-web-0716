7aa221d2a2777dde8f87fe058e08f4c72f0ce400
Troy Chryssos <github email address>
2016-08-19 17:40:40 -0400

    Wrote validations which check author and post values Wrote validations which check that a form is recieving valid data. It checks for the presence and uniqeness of author names, as well as a valid phone number. For posts, it checks that a title exists and includes click-bait keywords, as well as checking for summary and content length, as well as genre. It stops records from being saved if they do not adhere to given parameters.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
+# class ClickBaitValidator < ActiveModel::Validator
+#   def validate(post)
+#     click_bait=["Won't Believe", "Secret","Top \d", "Guess"]
+#     unless click_bait.any?{|bait| post.title.include? bait}
+#       post.errors[:title] << 'Must have a clickbait title!'
+#     end
+#   end
+# en
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :click_bait
+  # validates :title, inclusion: {in: ["Won't Believe", "Secret","Top \d", "Guess"]}
+
+  def click_bait
+    self.title ||= "Dang"
+    click_bait=["Won't Believe", "Secret","Top \d", "Guess"]
+    unless click_bait.any?{|bait| self.title.include? bait}
+      errors.add(:title, "Your title must be more click-baity!")
+    end
+  end
+
 end
